0d79b824bfaccd63bfbcac79d58294689e9586e0
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-05-18 09:52:30 +0700

    update model users :rocket:

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'username',
+        'no_hp',
         'email',
         'password',
     ];
