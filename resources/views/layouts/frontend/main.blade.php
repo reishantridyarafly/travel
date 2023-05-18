2821e812d0261807f24af9b99a3d094792bb1239
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-05-18 08:28:10 +0700

    setup spatie permission :rocket:

--- resources/views/layouts/frontend/main.blade.php
+++ resources/views/layouts/frontend/main.blade.php
@@ -1,13 +1,9 @@
-@php
-    $setting = App\Models\Setting::find(1);
-    $user = Illuminate\Support\Facades\Auth::user();
-@endphp
 <!doctype html>
 <html lang="en" dir="ltr">
 
     <head>
         <meta charset="utf-8" />
-        <title>{{ $setting->name }}</title>
+        <title>Travel</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Premium Bootstrap 5 Landing Page Template" />
@@ -18,7 +14,7 @@
         <meta name="Version" content="v4.2.0" />
 
         <!-- favicon -->
-        <link rel="shortcut icon" href="{{ asset('storage/settings/' . $setting->favicon ) }}" />
+        <link rel="shortcut icon" href="{{ asset('frontend') }}/images/favicon.ico" />
 
         <!-- Css -->
         <link href="{{ asset('frontend') }}/libs/tiny-slider/tiny-slider.css" rel="stylesheet">
@@ -51,26 +47,8 @@
         @include('layouts.frontend.footer')
 
         <div class="icons-wrapper">
-            @if (Auth::check())
-            @php
-                $count = App\Models\Chat::where('recipient_id', $user->id)
-                    ->where('status', 'unread')
-                    ->whereHas('sender', function ($query) use ($user) {
-                        $query->where('id', '!=', $user->id);
-                    })
-                    ->orderBy('created_at', 'ASC')
-                    ->get()
-                    ->count();
-            @endphp
-            @if ($count > 0)
-            {{-- <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger">{{ $count }}</span> --}}
-            @endif
-            <a href="#" id="chat-icon" class="chat-icon fs-5" data-bs-toggle="modal" data-bs-target="#chatModal"><i data-feather="message-circle" class="fea icon-sm icons align-middle"></i></a>
-            @endif
             <a href="#" onclick="topFunction()" id="back-to-top" class="back-to-top fs-5"><i data-feather="arrow-up" class="fea icon-sm icons align-middle"></i></a>
         </div>
-
-        @include('frontend.chat.index')
 
         <!-- Javascript -->
         <!-- JAVASCRIPT -->
@@ -79,8 +57,8 @@
         <script src="{{ asset('frontend') }}/libs/feather-icons/feather.min.js"></script>
         <!-- SLIDER -->
         <script src="{{ asset('frontend') }}/libs/tiny-slider/min/tiny-slider.js"></script>
+        <script src="{{ asset('frontend') }}/js/easy_background.js"></script>
         @yield('javascript')
-        @yield('javascript-chat')
         <!-- Main Js -->
         <script src="{{ asset('frontend') }}/js/plugins.init.js"></script><!--Note: All init js like tiny slider, counter, countdown, maintenance, lightbox, gallery, swiper slider, aos animation etc.-->
         <script src="{{ asset('frontend') }}/js/app.js"></script><!--Note: All important javascript like page loader, menu, sticky menu, menu-toggler, one page menu etc. -->
