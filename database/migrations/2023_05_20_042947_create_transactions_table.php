7528f2d463b8d7c0353ceafb4ee0448c053b51f3
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-05-20 13:05:16 +0700

    add migration packages, bookings, transactions, and payments :rocket:

---
+++ database/migrations/2023_05_20_042947_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('booking_id');
+            $table->string('name_bank');
+            $table->string('photo evidence');
+            $table->double('price');
+            $table->string('status')->default('pending');
+            $table->timestamps();
+            $table->foreign('booking_id')->references('id')->on('bookings')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
